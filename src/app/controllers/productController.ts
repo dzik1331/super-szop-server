4671ff4fff8d31fc7282637ad19f97e13db3a129
Krzysztof Nieradzik <krzynie1331@gmail.com>
2018-10-18 23:03:16 +0200

    Dodawanie produktu

--- src/app/controllers/productController.ts
+++ src/app/controllers/productController.ts
@@ -1,79 +1,26 @@
-import {ApiController, Controller, HttpGet} from 'dinoloop';
+import {ApiController, Controller, HttpGet, HttpPost} from 'dinoloop';
+import {ProductService} from "../services/product.service";
 
 @Controller('/product')
 export class ProductController extends ApiController {
 
-    constructor() {
+    constructor(private productService: ProductService) {
         super();
+    }
+
+    @HttpGet('/tags')
+    tagsTypes() {
+        return this.productService.getTags();
     }
 
     @HttpGet('/list')
     devices() {
-        return [
-            {
-                id: 1,
-                name: 'Produkt 1',
-                price: 25,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'https://bakoma.pl/wp-content/uploads/bio_truskawka_370x370-200x200.png',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            },
-            {
-                id: 2,
-                name: 'Produkt 2',
-                price: 501.45,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'http://www.aleradetails.com/wp-content/uploads/2016/08/238239-200x200.jpg',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            },
-            {
-                id: 3,
-                name: 'Produkt 3',
-                price: 49.99,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'http://superhitproducts.in/wp-content/uploads/2017/01/i-love-sundays-black-mug-31-200x200.jpg',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            },
-            {
-                id: 4,
-                name: 'Produkt 4',
-                price: 100,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'http://avoornetworks.com/wp-content/uploads/2016/10/wireless-cisco-200x200.png',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            },
-            {
-                id: 5,
-                name: 'Produkt 1',
-                price: 25,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'https://bakoma.pl/wp-content/uploads/bio_truskawka_370x370-200x200.png',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            },
-            {
-                id: 6,
-                name: 'Produkt 2',
-                price: 501.45,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'http://www.aleradetails.com/wp-content/uploads/2016/08/238239-200x200.jpg',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            },
-            {
-                id: 7,
-                name: 'Produkt 3',
-                price: 49.99,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'http://superhitproducts.in/wp-content/uploads/2017/01/i-love-sundays-black-mug-31-200x200.jpg',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            },
-            {
-                id: 8,
-                name: 'Produkt 4',
-                price: 100,
-                tags: ['elektronika', 'komputery Pc'],
-                imgSmall: 'http://avoornetworks.com/wp-content/uploads/2016/10/wireless-cisco-200x200.png',
-                description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
-            }
-        ];
+        return this.productService.products
+    }
+
+    @HttpPost('/add')
+    add(body) {
+        console.log(body)
+        this.productService.products.push(body);
     }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,76 @@
 import {Injectable} from "injection-js";
 
+interface Product {
+    id;
+    name;
+    price;
+    tags;
+    img;
+    description;
+}
+
 @Injectable()
 export class ProductService {
 
+    products: Product[] = [
+        {
+            id: 1,
+            name: 'Produkt 1',
+            price: 25,
+            tags: ['elektronika', 'komputery Pc'],
+            img: 'https://bakoma.pl/wp-content/uploads/bio_truskawka_370x370-200x200.png',
+            description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
+        },
+        {
+            id: 2,
+            name: 'Produkt 2',
+            price: 501.45,
+            tags: ['elektronika', 'komputery Pc'],
+            img: 'http://www.aleradetails.com/wp-content/uploads/2016/08/238239-200x200.jpg',
+            description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
+        },
+        {
+            id: 3,
+            name: 'Produkt 3',
+            price: 49.99,
+            tags: ['elektronika', 'komputery Pc'],
+            img: 'http://superhitproducts.in/wp-content/uploads/2017/01/i-love-sundays-black-mug-31-200x200.jpg',
+            description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
+        },
+        {
+            id: 4,
+            name: 'Produkt 4',
+            price: 100,
+            tags: ['elektronika', 'komputery Pc'],
+            img: 'http://avoornetworks.com/wp-content/uploads/2016/10/wireless-cisco-200x200.png',
+            description: 'Produkt najwyższej jakości, wyprodukowany w Chinach'
+        }
+    ];
+
+    getTags() {
+        return [
+            'Jedzenie',
+            'Elektronika',
+            'RTV AGD',
+            'Telewizor',
+            'Komputer',
+            'PC',
+            'Laptop',
+            'Notebook',
+            'Muzyka',
+            'Film',
+            'Bajka',
+            'Książka',
+            'Animacja',
+            'Warzywa',
+            'Owoce',
+            'Kuchnia',
+            'Sypialnia',
+            'Łazienka',
+            'Rozrywka',
+            'Gry',
+            'Meble',
+            'Remont'
+        ]
+    }
 }
